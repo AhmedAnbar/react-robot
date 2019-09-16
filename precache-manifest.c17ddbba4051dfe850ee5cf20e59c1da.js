d9ffbee861638731f14a81a2ce905b85b44dcee7
Ahmed Anber <begnulinux@gmail.com>
2019-09-16 02:19:18 +0200

    Updates

---
+++ precache-manifest.c17ddbba4051dfe850ee5cf20e59c1da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c3f009b3355fed6d5416aa02730d83",
+    "url": "/react-robot/index.html"
+  },
+  {
+    "revision": "8e014b0ac77c48f189e9",
+    "url": "/react-robot/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "84b53ad5dd40d11674d3",
+    "url": "/react-robot/static/css/main.0ed7ac39.chunk.css"
+  },
+  {
+    "revision": "8e014b0ac77c48f189e9",
+    "url": "/react-robot/static/js/2.4de3e7c3.chunk.js"
+  },
+  {
+    "revision": "84b53ad5dd40d11674d3",
+    "url": "/react-robot/static/js/main.30ffa61f.chunk.js"
+  },
+  {
+    "revision": "31cc2d9bff7c1d8a31c6",
+    "url": "/react-robot/static/js/runtime~main.5256f6db.js"
+  },
+  {
+    "revision": "58308c4c9da25fd77360ab5682ac1aac",
+    "url": "/react-robot/static/media/Modak-Regular.58308c4c.woff2"
+  },
+  {
+    "revision": "8fcd9b3d0ff6114211af9ce7567a6f9f",
+    "url": "/react-robot/static/media/Modak-Regular.8fcd9b3d.ttf"
+  },
+  {
+    "revision": "f84c2850515d2a96d28ebc2a2d11ebe5",
+    "url": "/react-robot/static/media/Modak-Regular.f84c2850.eot"
+  },
+  {
+    "revision": "72ea990e21a2efa086a3a23be0c35cd6",
+    "url": "/react-robot/static/media/Modak.72ea990e.svg"
+  },
+  {
+    "revision": "d5375880a99c68ba6b28966cbb287f76",
+    "url": "/react-robot/static/media/Modak.d5375880.woff"
+  }
+]);
